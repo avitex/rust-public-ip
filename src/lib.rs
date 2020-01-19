aea6dfee68c97580bd8ba793d003361b9d3e5914
avitex <theavitex@gmail.com>
2020-01-19 20:46:53 +1100

    cleanup doc examples

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,16 @@
 //! use async_std::task;
 //! use public_ip::{dns, http, ToResolver, BoxToResolver};
 //!
-//! fn main() {
-//!     // List of resolvers to try and get an IP address from
-//!     let resolver = vec![
-//!         BoxToResolver::new(dns::OPENDNS_RESOLVER),
-//!         BoxToResolver::new(http::HTTP_IPIFY_ORG_RESOLVER),
-//!     ].to_resolver();
-//!     // Attempt to get an IP address and print it
-//!     if let Some(ip) = task::block_on(public_ip::resolve_address(resolver)) {
-//!         println!("public ip address: {:?}", ip);
-//!     } else {
-//!         println!("couldn't get an IP address");
-//!     }
+//! // List of resolvers to try and get an IP address from
+//! let resolver = vec![
+//!     BoxToResolver::new(dns::OPENDNS_RESOLVER),
+//!     BoxToResolver::new(http::HTTP_IPIFY_ORG_RESOLVER),
+//! ].to_resolver();
+//! // Attempt to get an IP address and print it
+//! if let Some(ip) = task::block_on(public_ip::resolve_address(resolver)) {
+//!     println!("public ip address: {:?}", ip);
+//! } else {
+//!     println!("couldn't get an IP address");
 //! }
 //! ```
 //!
@@ -30,21 +28,19 @@
 //! use async_std::task;
 //! use public_ip::{dns, ToResolver, Resolution};
 //!  
-//! fn main() {
-//!     // List of resolvers to try and get an IP address from
-//!     let resolver = dns::OPENDNS_RESOLVER.to_resolver();
-//!     // Attempt to get an IP address and print it
-//!     if let Some(resolution) = task::block_on(public_ip::resolve(resolver)) {
-//!         if let Some(resolution) = Any::downcast_ref::<dns::DnsResolution>(&resolution) {
-//!             println!("public ip address {:?} resolved from {:?} ({:?})",
-//!                 resolution.address(),
-//!                 resolution.name(),
-//!                 resolution.server(),
-//!             );
-//!         }
-//!     } else {
-//!         println!("couldn't get an IP address");
+//! // List of resolvers to try and get an IP address from
+//! let resolver = dns::OPENDNS_RESOLVER.to_resolver();
+//! // Attempt to get an IP address and print it
+//! if let Some(resolution) = task::block_on(public_ip::resolve(resolver)) {
+//!     if let Some(resolution) = Any::downcast_ref::<dns::DnsResolution>(&resolution) {
+//!         println!("public ip address {:?} resolved from {:?} ({:?})",
+//!             resolution.address(),
+//!             resolution.name(),
+//!             resolution.server(),
+//!         );
 //!     }
+//! } else {
+//!     println!("couldn't get an IP address");
 //! }
 //! ```
 
