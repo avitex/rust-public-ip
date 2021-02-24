dbe1ee60252964e453cb8ccbaeb69bd9d73badef
avitex <theavitex@gmail.com>
2021-02-24 12:25:41 +1100

    add errors example

---
+++ examples/errors.rs
@@ -0,0 +1,24 @@
+use futures_util::{future, StreamExt, TryStreamExt};
+use public_ip::{self, dns, http, Version};
+
+#[tokio::main]
+async fn main() {
+    // List of resolvers to try and get an IP address from.
+    let resolver = &[
+        http::HTTP_WHATISMYIPADDRESS_COM_RESOLVER,
+        dns::GOOGLE_DNS_TXT_RESOLVER,
+    ];
+    let addr = public_ip::resolve_stream(resolver, Version::Any)
+        // For each error in the stream we print it out to STDERR (console).
+        .inspect_err(|err| eprintln!("resolver error: {}", err))
+        // We filter out the errors and leave just the resolved addresses in the stream.
+        .filter_map(|result| future::ready(result.ok()))
+        // We get the first resolved address in the stream.
+        .next()
+        // Wait for the future to finish.
+        .await
+        // We remove the details of the resolution if we don't care about them.
+        .map(|(addr, _details)| addr);
+
+    dbg!(addr);
+}
